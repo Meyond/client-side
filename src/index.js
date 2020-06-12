d3095101dd322f1532040e99b3b505c7a6d934ed
unknown <W9005993@adc.com>
2020-06-12 12:54:27 +0800

    使用类型断言代码同步

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
+"use strict";
 var h1 = document.createElement('h1');
 h1.innerHTML = 'Hello, I am Li';
 document.body.appendChild(h1);
-var obj = { name: 'Lison' };
-// console.log(obj.name)
-function getKeys(obj) {
-    return Object.keys(obj); // 会以列表的形式返回obj中的值
-}
-console.log(getKeys(obj));
+// ===========================================
+var merge = function (arg1, arg2) {
+    var res = {};
+    res = Object.assign(arg1, arg2);
+    return res;
+};
+var info1 = {
+    name: "lison"
+};
+var info2 = {
+    age: 18
+};
+var lisonInfo = merge(info1, info2);
+console.log(lisonInfo.age);
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 
-const h1 = document.createElement('h1')
-h1.innerHTML = 'Hello, I am Li'
-document.body.appendChild(h1)
+const h1 = document.createElement('h1');
+h1.innerHTML = 'Hello, I am Li';
+document.body.appendChild(h1);
 
-let obj: object = { name: 'Lison' }
-console.log(obj.name)
+// ===========================================
 
-function getKeys (obj: object) {
-  return Object.keys(obj) // 会以列表的形式返回obj中的值
-}
-console.log(getKeys(obj));
+const merge = <T, U>(arg1: T, arg2: U): T & U => {
+  let res = {} as T & U;
+  res = Object.assign(arg1, arg2);
+  return res;
+};
+
+const info1 = { name: [] };
+const info2 = { age: 18 };
+const lisonInfo = merge(info1, info2);
